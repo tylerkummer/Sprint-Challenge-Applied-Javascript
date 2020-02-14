17ec201378ca934106f70cb0a1c7ec07e9a50007
Tyler Kummer <tylerkummer@protonmail.com>
2020-02-14 12:00:03 -0600

    Initial Commit

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,51 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const ax2 = axios.get('https://lambda-times-backend.herokuapp.com/articles');
+
+ax2.then(response =>{
+    console.log(response.data);
+})
+
+const authorCredit = document.querySelector('.cards-container');
+ax2.then(response =>{
+    const credits = Object.values(response.data);
+    credits.forEach(element=>{
+        const credits2 = Object.values(element);
+        credits2.forEach(item=>{
+            const credits3 = Object.values(item);
+            item.forEach(item2=>{
+                authorCredit.append(NewCardTable(item2));
+            })
+        })
+    })
+})
+
+function NewCardTable(crd){
+    const newCard =  document.createElement('div');
+    const newHeadLine = document.createElement('div');
+    const newAuthor = document.createElement('div');
+    const newImgContainer = document.createElement('div');
+    const newImg = document.createElement('img');
+    const newAuthorName = document.createElement('span');
+
+    newCard.appendChild(newHeadLine);
+    newCard.appendChild(newAuthor);
+    newAuthor.appendChild(newImgContainer);
+    newImgContainer.appendChild(newImg);
+    newCard.appendChild(newAuthorName);
+
+    newCard.classList.add('card');
+    newHeadLine.classList.add('headline');
+    newAuthor.classList.add('author');
+    newImgContainer.classList.add('img-container');
+    newImg.classList.add('img');
+    newAuthorName.classList.add('span');
+
+    newHeadLine.textContent = crd.headline;
+    newImg.src = crd.authorPhoto;
+    newAuthorName.textContent = crd.authorName;
+
+    return newCard;
+}
